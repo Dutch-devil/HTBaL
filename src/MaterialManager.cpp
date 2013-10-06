1fd94ef03df67ad1b76feb22c0591c657eabe5e0
Dutch-devil <hero_lennart@hotmail.com>
2013-10-07 00:22:00 +0200

    fix properties fix

--- src/MaterialManager.cpp
+++ src/MaterialManager.cpp
@@ -7,8 +7,8 @@ RenderState::StateBlock* MaterialManager::stateBlock = NULL;
 
 Material* MaterialManager::getMaterial(MaterialType type) {
 	if (propertiesArray == NULL) {
-		propertiesArray = new Properties*[COUNT*2];
-		memset(propertiesArray, NULL, COUNT*2*sizeof(Properties*));
+		propertiesArray = new Properties*[COUNT];
+		memset(propertiesArray, NULL, COUNT*sizeof(Properties*));
 
 		stateBlock = RenderState::StateBlock::create();
 		//stateBlock->setCullFace(false);
@@ -16,42 +16,36 @@ Material* MaterialManager::getMaterial(MaterialType type) {
 		stateBlock->setBlendSrc(RenderState::BLEND_SRC_ALPHA);
 		stateBlock->setBlendDst(RenderState::BLEND_ONE_MINUS_SRC_ALPHA);
 	}
-	if(propertiesArray[type*2] == NULL) {
+	if(propertiesArray[type] == NULL) {
 		switch(type) {
 		case FLOOR:
-			propertiesArray[type*2+1] = Properties::create("res/house/floor.material");
-			propertiesArray[type*2] = fixProperties(propertiesArray[type*2+1]);
-			print("creating: %d at %d\n",propertiesArray[type], type);
+			propertiesArray[type] = Properties::create("res/house/floor.material");
 			break;
 		case WALL:
-			propertiesArray[type*2+1] = Properties::create("res/house/wall.material");
-			propertiesArray[type*2] = fixProperties(propertiesArray[type*2+1]);
-			print("creating: %d at %d\n",propertiesArray[type], type);
+			propertiesArray[type] = Properties::create("res/house/wall.material");
 			break;
 		case DOOR:
-			propertiesArray[type*2+1] = Properties::create("res/house/door.material");
-			propertiesArray[type*2] = fixProperties(propertiesArray[type*2+1]);
-			print("creating: %d at %d\n",propertiesArray[type], type);
+			propertiesArray[type] = Properties::create("res/house/door.material");
 			break;
 		default:
 			GP_ERROR("No matching material found");
 		}
 	}
 
-	Material* material = Material::create(propertiesArray[type*2]);
+	Material* material = Material::create(fixProperties(propertiesArray[type]));
 	material->setStateBlock(stateBlock);
 	return material;
 }
 
 Properties* MaterialManager::fixProperties(Properties* properties) {
+	properties->rewind();
 	return (strlen(properties->getNamespace()) > 0) ? properties : properties->getNextNamespace();
 }
 
 void MaterialManager::finalize(){
 	SAFE_RELEASE(stateBlock);
-	for(int i = 0; i < COUNT*2; i += 2) {
-		propertiesArray[i] = NULL;
-		SAFE_DELETE(propertiesArray[i+1]);
+	for (int i = 0; i < COUNT; i++) {
+		SAFE_DELETE(propertiesArray[i]);
 	}
 	SAFE_DELETE_ARRAY(propertiesArray);
 }
